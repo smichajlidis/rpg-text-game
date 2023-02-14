9e8b8511b94cccfd69144b819388be71cef54a4c
smichajlidis <smichajlidis@gmail.com>
2023-02-15 00:04:27 +0100

    Add Weapon class that inherits from Item class
    First derived class created.

--- includes.h
+++ includes.h
@@ -2,3 +2,6 @@
 
 #include "includes/item.h"
 #include "src/item.cpp"
+
+#include "includes/item/weapon.h"
+#include "src/item/weapon.cpp"
--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@ int main() {
     Item new_object;
     Item object {"object", 20, 12};
 
+    Weapon sword {"sword", 50, 45};
+    Weapon bow;
+
+    std::cout << sword.get_name() << std::endl;
+    std::cout << sword.get_price() << std::endl;
+    std::cout << sword.get_amount() << std::endl;
+
     std::cout << "Hello World!" << std::endl;
 
     return 0;
