60a028ce7c4e282a4a9a3af6de87fb163f46a89c
smichajlidis <smichajlidis@gmail.com>
2023-02-14 22:42:06 +0100

    Create Item class
    Item class is done. Now I have to create classes which inherit from this
    class.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include "includes/item.h"
+#include "src/item.cpp"
+
 int main() {
+
+    Item leather {"leather"};
+    Item new_object;
+    Item object {"object", 20, 12};
 
     std::cout << "Hello World!" << std::endl;
 
