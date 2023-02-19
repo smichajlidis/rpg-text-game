33d9a6a627a2bfc29da743807a8faf292587d75e
smichajlidis <smichajlidis@gmail.com>
2023-02-19 20:45:37 +0100

    display_location as a inherited method in Location class
    Small change: Location class have all 5 answers attributes that are inherited. There is a default value "none" in constructor.

--- includes/location.h
+++ includes/location.h
@@ -6,12 +6,17 @@ class Location {
 
     std::string name;
     std::string description;
+    std::string answer_1;
+    std::string answer_2;
+    std::string answer_3;
+    std::string answer_4;
+    std::string answer_5;
 
 public:
 
-    virtual void display_location()=0;
+    void display_location();
 
-    Location(std::string name_val = "nowhere", std::string description_val = "description");
+    Location(std::string name_val = "nowhere", std::string description_val = "description", std::string answer_1_val = "none", std::string answer_2_val = "none", std::string answer_3_val = "none", std::string answer_4_val = "none", std::string answer_5_val = "none");
     ~Location();
     
 };
--- src/location.cpp
+++ src/location.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include "../includes/location.h"
 
-Location::Location(std::string name_val, std::string description_val)
-    : name {name_val}, description {description_val} {
+void Location::display_location() {
+    std::cout<<description<<"\n\n";
+    if (answer_1!="none")
+        std::cout<<"1. "<<answer_1<<"\n";
+    if (answer_2!="none")
+        std::cout<<"2. "<<answer_2<<"\n";
+    if (answer_3!="none")
+        std::cout<<"3. "<<answer_3<<"\n";
+    if (answer_4!="none")
+        std::cout<< "4. "<<answer_4<<"\n";
+    if (answer_5!="none")
+        std::cout<< "5. "<<answer_5<<"\n";
+    std::cout<<"0. Return"<<"\n\n";
+    std::cout<<"What do you do? ";
+}
+
+Location::Location(std::string name_val, std::string description_val, std::string answer_1_val, std::string answer_2_val, std::string answer_3_val, std::string answer_4_val, std::string answer_5_val)
+    : name {name_val}, description {description_val}, answer_1 {answer_1_val}, answer_2 {answer_2_val}, answer_3 {answer_3_val}, answer_4 {answer_4_val}, answer_5 {answer_5_val} {
     }
 
 Location::~Location() {
