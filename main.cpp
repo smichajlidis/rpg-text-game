bff41d9ea44d4e22010e489928e0eaaa655c779c
smichajlidis <smichajlidis@gmail.com>
2023-02-21 21:26:16 +0100

    Add clear console function before each relocation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <cstdlib>
 #include "includes.h"
+
+void clear();
 
 int main() {
 
@@ -8,8 +11,21 @@ GameState game_state;
 int i {1};
 
 do {
+  clear();
   game_state.display_location();
   } while (i == 1);
 
   return 0;
+}
+
+void clear() {
+#if defined _WIN32
+    system("cls");
+    //clrscr(); // including header file : conio.h
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+#elif defined (__APPLE__)
+    system("clear");
+#endif
 }
