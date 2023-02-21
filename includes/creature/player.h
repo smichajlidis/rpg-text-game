6d673cfb27c19dd5539282a5551532956790bc18
smichajlidis <smichajlidis@gmail.com>
2023-02-21 22:03:38 +0100

    Add attribute "drunk" in Player class
    It will increase when you drink beer. It will gives some advantages.

--- includes/creature/player.h
+++ includes/creature/player.h
@@ -6,10 +6,11 @@ class Player: public Creature {
 
     int charisma;
     int luck;
+    int drunk;
 
 public:
 
-    Player(std::string name_val = "none", int strength_val = 1, int hp_val = 100, int charisma_val = 1, int luck_val = 1);
+    Player(std::string name_val = "none", int strength_val = 1, int hp_val = 100, int charisma_val = 1, int luck_val = 1, int drunk_val = 0);
     ~Player();
 
 };
--- src/creature/player.cpp
+++ src/creature/player.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "../../includes/creature/player.h"
 
-Player::Player(std::string name_val, int strength_val, int charisma_val, int luck_val, int hp_val)
-    : Creature(name_val, strength_val, hp_val), charisma {charisma_val}, luck {luck_val} {
+Player::Player(std::string name_val, int strength_val, int charisma_val, int luck_val, int hp_val, int drunk_val)
+    : Creature(name_val, strength_val, hp_val), charisma {charisma_val}, luck {luck_val}, drunk {drunk_val} {
     }
 
 Player::~Player() {
