a52375594134fd28239007686558ddade4edc522
smichajlidis <smichajlidis@gmail.com>
2023-02-24 10:35:20 +0100

    Cleaning up

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <cstdlib>
 #include "includes.h"
 
-//void clear();
-
 int main() {
 
 GameState game_state;
@@ -18,15 +16,3 @@ do {
 
   return 0;
 }
-
-//void clear() {
-//#if defined _WIN32
-//    system("cls");
-//    //clrscr(); // including header file : conio.h
-//#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
-//    system("clear");
-//    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
-//#elif defined (__APPLE__)
-//    system("clear");
-//#endif
-//}
