c491666a64fa064c76492584fa8dcca1a884a0f2
smichajlidis <smichajlidis@gmail.com>
2023-03-02 00:41:12 +0100

    Add if in "increase_hp" function to prevent hp increasing above 100

--- src/creature.cpp
+++ src/creature.cpp
@@ -3,7 +3,10 @@
 #include "../includes/equipment.h"
 
 void Creature::increase_hp(int hp_val) {
-    hp+=hp_val;
+    if (hp+hp_val<=100)
+        hp+=hp_val;
+    else 
+        hp=100;
 }
 
 Creature::Creature(std::string name_val, int strength_val, int hp_val)
