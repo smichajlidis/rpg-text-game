f4cdb8f53b338a57a1906db8eec3e9f0614fa33d
smichajlidis <smichajlidis@gmail.com>
2023-03-12 21:26:05 +0100

    Add using charisma with thugs

--- includes/location/charisma_trial.h
+++ includes/location/charisma_trial.h
@@ -1,13 +1,15 @@
 #ifndef _CHARISMATRIAL_H_
 #define _CHARISMATRIAL_H_
 
-class CharismaTrial: public Location {
+class CharismaTrial: public Location, public NPC {
 
 public:
 
     virtual Location* making_a_choice();
+    virtual void greeting();
+    virtual Creature* clone();
 
-    CharismaTrial(std::string name_val = "charisma_trial");
+    CharismaTrial(std::string name_val = "THUGS");
     ~CharismaTrial();
 
 };
--- src/location/charisma_trial.cpp
+++ src/location/charisma_trial.cpp
@@ -2,15 +2,35 @@
 #include "../../includes/location/charisma_trial.h"
 
 Location* CharismaTrial::making_a_choice() {
+    display_npc_details();
+    std::cout<<"\n";
     char choice {};
-    std::cout<<"Charisma trial...\n\n";
-    std::cout<<"Press any key to continue: ";
-    std::cin>>choice;
+    if ((*player).using_charisma(favor)) {
+        std::cout<<"\nSuccess, you got this!\n";
+        (*player).increase_charisma(1);
+        increase_favor();
+    }
+    else {
+        std::cout<<"\nNo... that was wrong.\n";
+        decrease_favor();
+    }
+    std::cout<<"\nPress any key to continue: ";
+    std::cin>>choice; 
     return related_locations.at(0);
 }
 
+void CharismaTrial::greeting() {
+}
+
+Creature* CharismaTrial::clone() {
+    Creature* thugs {nullptr};
+    thugs = new CharismaTrial;
+    return thugs;
+}
+
 CharismaTrial::CharismaTrial(std::string name_val)
-    : Location(name_val) {
+    : Location(name_val), NPC(name_val) {
+        favor=20;
 }
 
 CharismaTrial::~CharismaTrial() {
