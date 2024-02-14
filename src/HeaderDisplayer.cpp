a05c4cdf7cdb7b63644fbb331c61866c0ad38d74
smichajlidis <smichajlidis@gmail.com>
2024-02-14 14:03:31 +0100

    Add one more \n after after header

--- src/HeaderDisplayer.cpp
+++ src/HeaderDisplayer.cpp
@@ -6,5 +6,5 @@ void HeaderDisplayer::displayHeader() const {
     std::cout << std::endl;
     std::cout << "\t..........................................\n";
     std::cout << "\t------------ SUPER LEGS 2000 -------------\n";
-    std::cout << "\t``````````````````````````````````````````" << std::endl;
+    std::cout << "\t``````````````````````````````````````````\n" << std::endl;
 }
