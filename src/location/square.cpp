4fed95e5033fbed888d13c4572f51afed5564c50
smichajlidis <smichajlidis@gmail.com>
2024-02-16 11:11:56 +0100

    Delete square.cpp file, put ctor and dtor in a header file. Clean up there

--- src/location/square.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../../include/location/square.h"
-
-Square::Square(std::string name_val, std::string description_val, std::string choice_1_val, std::string choice_2_val, std::string choice_3_val)
-    : Location(name_val, description_val, choice_1_val, choice_2_val, choice_3_val) {}
-
-Square::~Square() {
-}
-
