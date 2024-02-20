57bb41fd386ef2485ada44e3c2d279967d51e68b
smichajlidis <smichajlidis@gmail.com>
2024-02-20 15:28:45 +0100

    Delete commented code and clean everything up

--- include/game_state.h
+++ include/game_state.h
@@ -1,62 +1,38 @@
 #ifndef _GAME_STATE_H_
 #define _GAME_STATE_H_
 
-#include "creature/player.h"
-#include "creature/thug.h"
-#include "location/innkeeper.h"
-#include "location/gamblers.h"
-#include "location/ladies.h"
-#include "location/priest.h"
-#include "location/thugs.h"
-#include "location/altars.h"
-#include "location/order_beer.h"
-#include "location/trade.h"
-#include "location/buying.h"
-#include "location/selling.h"
-#include "location/gossip.h"
-#include "location/hot_dish.h"
-#include "equipment.h"
-#include "location/forest_exploration.h"
-#include "location/found_food.h"
-#include "location/hunting.h"
-#include "location/pay_for_entrance.h"
-#include "location/attack_thugs.h"
-#include "location/charisma_trial.h"
-#include "location/chapels_door.h"
-#include "location/strength_altar.h"
-#include "location/charisma_altar.h"
-#include "location/gold_altar.h"
-#include "location/weapon_altar.h"
-#include "location/dungeons.h"
-#include "location/dungeons_attack.h"
-#include "location/found_treasures.h"
-#include "location/dungeons_nothing.h"
-#include "item/chapel_key.h"
-#include "item/leather.h"
-#include "item/meat.h"
-#include "location/forest_attack.h"
-#include "location/found_nothing.h"
-#include "location/luck_altar.h"
-#include "screen_stuff.h"
-
 #include "location/Square.hpp"
 #include "location/Tavern.hpp"
+#include "location/ladies.h"
+#include "location/innkeeper.h"
+#include "location/gamblers.h"
+#include "location/priest.h"
+#include "location/altars.h"
+#include "location/dungeons.h"
 #include "location/Chapel.hpp"
 #include "location/Forest.hpp"
+#include "location/forest_exploration.h"
+#include "location/thugs.h"
 
+#include "screen_stuff.h"
 #include "SavingMenu.hpp"
 #include "LoadingMenu.hpp"
 #include "ExitingMenu.hpp"
+#include "equipment.h"
+
+#include "creature/player.h"
 
 #include <memory>
 
-class Sword;
-class Bow;
-class ChapelKey;
-class PointPlayer;
-class Priest;
-
 class GameState {
+public:
+    GameState();
+
+    ~GameState() = default;
+
+    void display_location();
+
+private:
 
     int i_ptr;
 
@@ -68,15 +44,9 @@ class GameState {
     SavingMenu saving_menu;
     LoadingMenu loading_menu;
     ExitingMenu exiting_menu;
-    PointPlayer point_player;
-
-    // NPC's
-
-    //Priest priest;
-
-    // Locations
-
     std::shared_ptr<Location> current_location;
+ 
+    // Locations
 
     std::shared_ptr<Square> square;
         std::shared_ptr<Tavern> tavern;
@@ -90,57 +60,6 @@ class GameState {
         std::shared_ptr<Chapel> chapel;
             std::shared_ptr<Altars> altars;
             std::shared_ptr<Dungeons> dungeons;
-
-    //     //Tavern tavern;
-    //         Innkeeper innkeeper;
-    //             OrderBeer order_beer;
-    //             Trade trade;
-    //                 Buying buying;
-    //                 Selling selling;
-    //             Gossip gossip;
-    //             HotDish hot_dish;
-    //         Gamblers gamblers;
-    //     // Priest priest;
-    //         Ladies ladies;
-    // //Forest forest;
-    //     ForestExploration forest_exploration;
-    //         ForestAttack forest_attack;
-    //         FoundFood found_food;
-    //         FoundNothing found_nothing;
-    //         Hunting hunting;
-    //     Thugs thugs;
-    //         PayForEntrance pay_for_entrance;
-    //         AttackThugs attack_thugs;
-    //         CharismaTrial charisma_trial;
-    // ChapelsDoor chapels_door;
-    //     //Chapel chapel;
-    //         Altars altars;
-    //             StrengthAltar strength_altar;
-    //             CharismaAltar charisma_altar;
-    //             LuckAltar luck_altar;
-    //             GoldAltar gold_altar;
-    //             WeaponAltar weapon_altar;    
-    //         Dungeons dungeons;
-    //             DungeonsAttack dungeons_attack;
-    //             FoundTreasures found_treasures;
-    //             DungeonsNothing dungeons_nothing;
-
-    // Items
-
-    ChapelKey chapel_key;
-    Leather leather;
-    Meat meat;
-
-    
-
-public:
-
-    void display_location();
-    //void get_i_ptr(int* i);
-
-    GameState();
-    ~GameState();
-
 };
 
 #endif
--- src/game_state.cpp
+++ src/game_state.cpp
@@ -1,11 +1,34 @@
+#include "../include/game_state.h"
+
 #include <iostream>
 
-#include "../include/game_state.h"
-#include "../include/item/food/forest_fruits.h"
-#include "../include/creature/skeleton.h"
-#include "../include/creature/wolf.h"
-#include "../include/item/weapon/hunting_weapon/bow.h"
-#include "../include/item/weapon/sword.h"
+GameState::GameState()
+    : i_ptr(1) {
+
+    square = std::make_shared<Square>();
+    tavern = std::make_shared<Tavern>(); 
+    forest = std::make_shared<Forest>(); 
+    chapel = std::make_shared<Chapel>();
+    square->getRelatedLocations(tavern, forest, chapel);
+
+    innkeeper = std::make_shared<Innkeeper>();
+    gamblers = std::make_shared<Gamblers>(); 
+    priest = std::make_shared<Priest>(); 
+    ladies = std::make_shared<Ladies>();
+    ladies->getRelatedLocations(tavern);
+    tavern->getRelatedLocations(innkeeper, gamblers, priest, ladies, square);
+    tavern->getRelatedLocations(tavern);
+
+    forest_exploration = std::make_shared<ForestExploration>();
+    thugs = std::make_shared<Thugs>(); 
+    forest->getRelatedLocations(forest_exploration, thugs, square);
+
+    altars = std::make_shared<Altars>();
+    dungeons = std::make_shared<Dungeons>(); 
+    chapel->getRelatedLocations(altars, dungeons, square);
+
+    current_location = square;
+}
 
 void GameState::display_location() {
     std::uint32_t choice;
@@ -48,214 +71,4 @@ void GameState::display_location() {
         screen_stuff.clear();
 
     } while(choice != 9);
-}
-
-GameState::GameState()
-    : i_ptr(1) {
-
-    square = std::make_shared<Square>();
-    tavern = std::make_shared<Tavern>(); 
-    forest = std::make_shared<Forest>(); 
-    chapel = std::make_shared<Chapel>();
-    square->getRelatedLocations(tavern, forest, chapel);
-
-    innkeeper = std::make_shared<Innkeeper>();
-    gamblers = std::make_shared<Gamblers>(); 
-    priest = std::make_shared<Priest>(); 
-    ladies = std::make_shared<Ladies>();
-    ladies->getRelatedLocations(tavern);
-    tavern->getRelatedLocations(innkeeper, gamblers, priest, ladies, square);
-    tavern->getRelatedLocations(tavern);
-    
-
-    forest_exploration = std::make_shared<ForestExploration>();
-    thugs = std::make_shared<Thugs>(); 
-    forest->getRelatedLocations(forest_exploration, thugs, square);
-
-    altars = std::make_shared<Altars>();
-    dungeons = std::make_shared<Dungeons>(); 
-    chapel->getRelatedLocations(altars, dungeons, square);
-
-    current_location = square;
-
-    // chapels_door.related_locations.push_back(&square);
-    // chapels_door.related_locations.push_back(&chapel);
-    // chapels_door.player_pointer(&player);
-
-    // innkeeper.related_locations.push_back(&tavern);
-    // innkeeper.related_locations.push_back(&order_beer);
-    // innkeeper.related_locations.push_back(&trade);
-    // innkeeper.related_locations.push_back(&gossip);
-    // innkeeper.related_locations.push_back(&hot_dish);
-    // innkeeper.related_locations.push_back(&innkeeper);
-    // innkeeper.player_pointer(&player);
-
-    // gamblers.related_locations.push_back(&tavern);
-    // gamblers.related_locations.push_back(&gamblers);
-    // gamblers.point_player(&player);
-    // gamblers.player_pointer(&player);
-
-    // ladies.related_locations.push_back(&tavern);
-    // ladies.related_locations.push_back(&ladies);
-    // ladies.point_player(&player);
-    // ladies.player_pointer(&player);
-
-    // priest.related_locations.push_back(&tavern);
-    // priest.related_locations.push_back(&priest);
-    // priest.point_player(&player);
-    // Item* chapel_key {nullptr};
-    // chapel_key = new ChapelKey;
-    // priest.equipment.items.push_back(chapel_key);
-    // priest.player_pointer(&player);
-
-    // thugs.related_locations.push_back(&forest);
-    // thugs.related_locations.push_back(&pay_for_entrance);
-    // thugs.related_locations.push_back(&charisma_trial);
-    // thugs.related_locations.push_back(&attack_thugs);
-    // thugs.related_locations.push_back(&thugs);
-    // thugs.player_pointer(&player);
-
-    //     pay_for_entrance.related_locations.push_back(&thugs);
-    //     pay_for_entrance.player_pointer(&player);
-    //     pay_for_entrance.get_i_ptr(&i_ptr);
-        
-    //     charisma_trial.related_locations.push_back(&thugs);
-    //     charisma_trial.player_pointer(&player);
-    //     charisma_trial.get_i_ptr(&i_ptr);
-
-    //     attack_thugs.related_locations.push_back(&thugs);
-    //     attack_thugs.player_pointer(&player);
-    //     Creature* thug {nullptr};
-    //     thug = new Thug;
-    //     attack_thugs.related_creatures.push_back(thug);
-    //     attack_thugs.get_i_ptr(&i_ptr);
-
-    // altars.related_locations.push_back(&chapel);
-    // altars.related_locations.push_back(&strength_altar);
-    // altars.related_locations.push_back(&charisma_altar);
-    // altars.related_locations.push_back(&luck_altar);
-    // altars.related_locations.push_back(&gold_altar);
-    // altars.related_locations.push_back(&weapon_altar);
-    // altars.player_pointer(&player);
-
-    //     strength_altar.related_locations.push_back(&altars);
-    //     strength_altar.player_pointer(&player);
-
-    //     charisma_altar.related_locations.push_back(&altars);
-    //     charisma_altar.player_pointer(&player);
-
-    //     luck_altar.related_locations.push_back(&altars);
-    //     luck_altar.player_pointer(&player);
-
-    //     gold_altar.related_locations.push_back(&altars);
-    //     gold_altar.player_pointer(&player);
-
-    //     weapon_altar.related_locations.push_back(&altars);
-    //     weapon_altar.player_pointer(&player);
-
-    // order_beer.point_player(&player);
-    // order_beer.related_locations.push_back(&innkeeper);
-    // order_beer.player_pointer(&player);
-
-    // trade.related_locations.push_back(&innkeeper);
-    // trade.related_locations.push_back(&buying);
-    // trade.related_locations.push_back(&selling);
-    // trade.related_locations.push_back(&trade);
-    // trade.player_pointer(&player);
-
-    // selling.point_equipment(&player.equipment);
-    // selling.point_trade_goods(&trade_goods);
-    // selling.related_locations.push_back(&trade);
-    // selling.related_locations.push_back(&selling);
-    // selling.player_pointer(&player);
-
-    // buying.point_equipment(&player.equipment);
-    // buying.point_trade_goods(&trade_goods);
-    // buying.related_locations.push_back(&trade);
-    // buying.related_locations.push_back(&buying);
-    // buying.player_pointer(&player);
-
-    // gossip.related_locations.push_back(&innkeeper);
-    // gossip.related_locations.push_back(&gossip);
-    // gossip.player_pointer(&player);
-
-    // hot_dish.related_locations.push_back(&innkeeper);
-    // hot_dish.point_player(&player);
-    // hot_dish.player_pointer(&player);
-
-    // forest_exploration.related_locations.push_back(&forest);
-    // forest_exploration.related_locations.push_back(&forest_attack);
-    // forest_exploration.related_locations.push_back(&found_food);
-    // forest_exploration.related_locations.push_back(&found_nothing);
-    // forest_exploration.related_locations.push_back(&hunting);
-    // forest_exploration.related_locations.push_back(&forest_exploration);
-    // forest_exploration.player_pointer(&player);
-
-    // dungeons.related_locations.push_back(&chapel);
-    // dungeons.related_locations.push_back(&dungeons_attack);
-    // dungeons.related_locations.push_back(&found_treasures);
-    // dungeons.related_locations.push_back(&dungeons_nothing);
-    // dungeons.related_locations.push_back(&chapel);
-    // dungeons.related_locations.push_back(&dungeons);
-    // dungeons.player_pointer(&player);
-
-    // forest_attack.related_locations.push_back(&forest_exploration);
-    // Creature* wolf {nullptr};
-    // wolf = new Wolf;
-    // forest_attack.related_creatures.push_back(wolf);
-    // forest_attack.player_pointer(&player);
-    // forest_attack.get_i_ptr(&i_ptr);
-
-    // found_food.related_locations.push_back(&forest_exploration);
-    // Item* forest_fruits {nullptr};
-    // forest_fruits = new ForestFruits;
-    // found_food.related_items.push_back(forest_fruits);
-    // found_food.player_pointer(&player);
-    
-    // found_nothing.related_locations.push_back(&forest_exploration);
-    // found_nothing.player_pointer(&player);
-
-    // hunting.related_locations.push_back(&forest_exploration);
-    // hunting.player_pointer(&player);
-    // hunting.related_items.push_back(&leather);
-    // hunting.related_items.push_back(&meat);
-
-    // dungeons_attack.related_locations.push_back(&dungeons);
-    // dungeons_attack.player_pointer(&player);
-    // Creature* skeleton {nullptr};
-    // skeleton = new Skeleton;
-    // dungeons_attack.related_creatures.push_back(skeleton);
-    // dungeons_attack.get_i_ptr(&i_ptr);
-
-    // found_treasures.related_locations.push_back(&dungeons);
-    // Item* bow2 {nullptr};
-    // bow2 = new Bow;
-    // found_treasures.related_items.push_back(bow2);
-    // found_treasures.player_pointer(&player);
-
-    // dungeons_nothing.related_locations.push_back(&dungeons);
-    // dungeons_nothing.player_pointer(&player);
-
-    // ADDING ITEMS FOR TRADING
-  
-    Item* sword2 {nullptr};
-    sword2 = new Sword;
-    player.equipment.items.push_back(sword2);
-
-    Item* sword {nullptr};
-    sword = new Sword;
-    trade_goods.items.push_back(sword);
-
-    (*(trade_goods.items).at(0)).increase_amount(2);
-
-    Item* bow {nullptr};
-    bow = new Bow;
-    trade_goods.items.push_back(bow);
-
-
-
-
-}
-
-GameState::~GameState() {
 }
