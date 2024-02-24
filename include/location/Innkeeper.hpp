9e259753ec22ba7c8d199ae7f2e3f02e66d5b2fd
smichajlidis <smichajlidis@gmail.com>
2024-02-24 21:51:01 +0100

    Add a description to going back to Tavern from Innkeeper classes

--- include/location/Innkeeper.hpp
+++ include/location/Innkeeper.hpp
@@ -10,8 +10,9 @@ public:
         const std::string& choice_1 = "Ask for a beer",
         const std::string& choice_2 = "Trade",
         const std::string& choice_3 = "Ask for a gossip",
-        const std::string& choice_4 = "Order something to eat")
-        : Location(description, choice_1, choice_2, choice_3, choice_4) {}
+        const std::string& choice_4 = "Order something to eat",
+        const std::string& choice_5 = "Say goodbye")
+        : Location(description, choice_1, choice_2, choice_3, choice_4, choice_5) {}
 
     ~Innkeeper() = default;
 };
