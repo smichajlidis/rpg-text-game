682547740b06eda95b6697212fa6002201ad27d8
smichajlidis <smichajlidis@gmail.com>
2024-02-24 21:47:46 +0100

    Correct descriptions

--- include/location/Tavern.hpp
+++ include/location/Tavern.hpp
@@ -6,11 +6,11 @@
 class Tavern: public Location {
 
 public:
-    Tavern(const std::string& description = "You are standing inside the dark tavern.",
-    const std::string& choice_1 = "Go to the inkeeper",
-    const std::string& choice_2 = "Go to the gamblers",
-    const std::string& choice_3 = "Go to the priest",
-    const std::string& choice_4 = "Go to the ladies",
+    Tavern(const std::string& description = "You are standing inside a dark tavern.",
+    const std::string& choice_1 = "Go to the counter and talk to the innkeeper",
+    const std::string& choice_2 = "Sit at the table with the gamblers",
+    const std::string& choice_3 = "Approach the priest sitting alone",
+    const std::string& choice_4 = "Go to ladies looking at you flirtatiously",
     const std::string& choice_5 = "Go out")
     : Location(description, choice_1, choice_2, choice_3, choice_4, choice_5) {}
     
