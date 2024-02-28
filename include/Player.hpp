334af28a5e9c973dea2b0009c37068f43165e1c1
smichajlidis <smichajlidis@gmail.com>
2024-02-28 17:34:53 +0100

    add charisma and luck values and inherits the class from Human

--- include/Player.hpp
+++ include/Player.hpp
@@ -1,14 +1,21 @@
 #ifndef _PLAYER_HPP_
 #define _PLAYER_HPP_
 
-#include "LivingBeing.hpp"
+#include "Human.hpp"
 
-class Player: public LivingBeing {
+class Player: public Human {
 
 public:
     Player() = default;
 
     ~Player() = default;
+
+    std::uint16_t getCharisma() const;
+    std::uint16_t getLuck() const;
+
+private:
+    std::uint16_t charisma;
+    std::uint16_t luck;
 };
 
 #endif
---
+++ src/Player.cpp
@@ -0,0 +1,9 @@
+#include "../include/Player.hpp"
+
+std::uint16_t Player::getCharisma() const {
+    return charisma;
+}
+
+std::uint16_t Player::getLuck() const {
+    return luck;
+}
