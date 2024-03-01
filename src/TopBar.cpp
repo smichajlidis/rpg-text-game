4fc830a323e7345352a3940d61c5a18cd1a051b9
smichajlidis <smichajlidis@gmail.com>
2024-03-01 15:31:29 +0100

    Delete unnecessary comments

--- src/TopBar.cpp
+++ src/TopBar.cpp
@@ -7,17 +7,11 @@ void TopBar::displayTopBar() const {
     std::cout << "________________________________________________________________________\n";
     std::cout << "\n      Equipment <6> | Load <7> | Save <8> | Quit Game <9>\n";
     std::cout << "________________________________________________________________________\n";
-  //  std::cout<<"------------------------------------------------------------------------\n\n";
-    std::cout << "    Hp: " << player->getHP(); //if(drunk>0) std::cout<<" -"<<drunk*2;
+
+    std::cout << "    Hp: " << player->getHP(); 
     std::cout << " | Gold: " << player->getGold();
-    std::cout << " | Strength: " << player->getStrength(); //if(drunk>0) std::cout<<" +"<<drunk; if (active_weapon) std::cout<<" +"<<(*active_weapon).return_value();
-    std::cout << " | Charisma: " << player->getCharisma(); //if(drunk>0) std::cout<<" +"<<drunk;
-    std::cout << " | Luck: " << player->getLuck(); //if(drunk>0) std::cout<<" +"<<drunk/2;
-    // std::cout<< "\n Active weapon: ";
-    // if (active_weapon)
-    //     std::cout<<(*active_weapon).return_name();
-    // else
-    //     std::cout<<"none";
+    std::cout << " | Strength: " << player->getStrength();
+    std::cout << " | Charisma: " << player->getCharisma(); 
     std::cout<<"\n\n\n";
 }
 
