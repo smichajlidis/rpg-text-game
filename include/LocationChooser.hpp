5f6420856634151d21f1f2628e8429c820719c5e
smichajlidis <smichajlidis@gmail.com>
2024-03-01 15:20:21 +0100

    Create LocationChooser class to generate a new location when a condition is needed

---
+++ include/LocationChooser.hpp
@@ -0,0 +1,18 @@
+#ifndef _LOCATIONCHOOSER_HPP_
+#define _LOCATIONCHOOSER_HPP_
+
+#include "Location.hpp"
+#include "Player.hpp"
+
+#include <memory>
+
+class LocationChooser: public Location {
+
+public:
+    LocationChooser() = default;
+
+    ~LocationChooser() = default;
+
+};
+
+#endif
