c414521d25cf1b134a5e129e50596ae1022a9602
smichajlidis <smichajlidis@gmail.com>
2024-03-07 15:07:16 +0100

    Make a distance between items and related locations

--- src/location/Buying.cpp
+++ src/location/Buying.cpp
@@ -5,6 +5,7 @@
 void Buying::printLocation() {
     std::cout << descriptions.at(0) << "\n\n";
     printEquipment();
+    std::cout << std::endl;
     std::cout << equipment.size() + 1 << ". " << descriptions.at(1) << std::endl;
     std::cout << equipment.size() + 2 << ". " << descriptions.at(2) << std::endl;
     std::cout << "\nWhat do you want?";
