bff328ed94f17206212fc76a5118703064dc539a
smichajlidis <smichajlidis@gmail.com>
2024-03-12 15:17:31 +0100

    Add attitude_to_player atribute and show it when you are talking with NPC

--- include/NPC.hpp
+++ include/NPC.hpp
@@ -10,7 +10,7 @@ class NPC: public LivingBeing, public Location {
 
 public:
     NPC(const std::string& description = "", const std::string& choice_1 = "", const std::string& choice_2 = "", const std::string& choice_3 = "", const std::string&choice_4 = "", const std::string& choice_5 = "")
-        : Location(description, choice_1, choice_2, choice_3, choice_4, choice_5) {}
+        : Location(description, choice_1, choice_2, choice_3, choice_4, choice_5) { attitude_to_player = 40; }
 
     ~NPC() = default;
 
@@ -18,6 +18,7 @@ public:
     
 protected:
     std::vector<std::string> greetings;
+    std::uint16_t attitude_to_player;
 };
 
 #endif
--- src/NPC.cpp
+++ src/NPC.cpp
@@ -4,6 +4,7 @@
 
 void NPC::printLocation() {
     srand(time(NULL));
+    std::cout << "[ATTITUDE: " << attitude_to_player << "/100]\n\n ";
     std::cout<<greetings.at(std::rand() % greetings.size());
     
     this->printDescriptions();
