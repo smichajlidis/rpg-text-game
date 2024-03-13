70096319efb85889d3749329f721b65dc5363d60
smichajlidis <smichajlidis@gmail.com>
2024-03-13 14:31:13 +0100

    Change to uppercase #ifndef and #define values

--- include/InteractionWithNPC.hpp
+++ include/InteractionWithNPC.hpp
@@ -1,5 +1,5 @@
-#ifndef _InteractionWithNPC_HPP_
-#define _InteractionWithNPC_HPP_
+#ifndef _INTERACTIONWITHNPC_HPP_
+#define _INTERACTIONWITHNPC_HPP_
 
 #include "LivingBeing.hpp"
 #include "Location.hpp"
@@ -11,17 +11,14 @@ class InteractionWithNPC: public LivingBeing, public Location {
 
 public:
     InteractionWithNPC(std::shared_ptr<Player> player, const std::string& description = "", const std::string& choice_1 = "", const std::string& choice_2 = "", const std::string& choice_3 = "", const std::string&choice_4 = "", const std::string& choice_5 = "")
-        : player(std::move(player)), Location(description, choice_1, choice_2, choice_3, choice_4, choice_5) { attitude_to_player = 40; }
+        : player(std::move(player)), Location(description, choice_1, choice_2, choice_3, choice_4, choice_5) {}
 
     ~InteractionWithNPC() = default;
 
     void printLocation() override;
-    void increaseAttitudeToPlayer(std::uint16_t);
-    void decreaseAttitudeToPlayer(std::uint16_t);
     
 protected:
     std::vector<std::string> sentences;
-    std::uint16_t attitude_to_player;
     std::shared_ptr<Player> player;
 };
 
