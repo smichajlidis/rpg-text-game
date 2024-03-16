13b7e64ed5732a44ae2f11771ac0b1c7e0d30e71
smichajlidis <smichajlidis@gmail.com>
2024-03-16 18:17:06 +0100

    Move chapel_key when attitute is greater or equal 80

--- src/location/AskingAboutChapel.cpp
+++ src/location/AskingAboutChapel.cpp
@@ -2,6 +2,10 @@
 
 std::shared_ptr<Location> AskingAboutChapel::moveToLocation(std::uint32_t val) {
         if (npc->getAttitudeToPlayer() >= 80) {
+            Item temp = npc->moveItem("chapel_key");
+            if (!temp.empty()) {
+                player->addItem(temp);
+            }
             return locations.at(1);
         } else {
             npc->decreaseAttitudeToPlayer(20);
