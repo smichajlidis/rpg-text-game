9f213d017665bbac07c7a360dfc875171aec1f32
smichajlidis <smichajlidis@gmail.com>
2024-03-20 14:00:58 +0100

    Relate in ctor with proper locations and npc

--- include/location/Buying.hpp
+++ include/location/Buying.hpp
@@ -6,10 +6,10 @@
 class Buying: public NPCTrade {
 
 public:
-    Buying(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state = nullptr, const std::string& description = "This is all I have:",
+    Buying(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state = nullptr,
+        const std::string& description = "This is all I have:",
         const std::string& choice_1 = "I want to sell you something now",
-        const std::string& choice_2 = "Return")
-        : NPCTrade(player, game_state, description, choice_1, choice_2) {}
+        const std::string& choice_2 = "Return");
 
     ~Buying() = default;
 
--- src/location/Buying.cpp
+++ src/location/Buying.cpp
@@ -2,6 +2,15 @@
 
 #include <iostream>
 
+Buying::Buying(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state, const std::string& description, const std::string& choice_1, const std::string& choice_2)
+    : NPCTrade(player, game_state, description, choice_1, choice_2) {
+
+    npc = game_state->getNPC("innkeeper");
+
+    related_locations.push_back("selling");
+    related_locations.push_back("approaching_innkeeper");
+}
+
 void Buying::printLocation() {
     std::cout << descriptions.at(0) << "\n\n";
     printEquipment();
