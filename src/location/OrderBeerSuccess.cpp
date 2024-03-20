b7a034d8ee9a5e8588625a34b5338d34e743982f
smichajlidis <smichajlidis@gmail.com>
2024-03-20 13:54:02 +0100

    Delete unnecessary includes

--- src/location/OrderBeerSuccess.cpp
+++ src/location/OrderBeerSuccess.cpp
@@ -1,8 +1,4 @@
 #include "../../include/location/OrderBeerSuccess.hpp"
-#include "../../include/location/OrderBeer.hpp"
-#include "../../include/location/Trade.hpp"
-#include "../../include/location/Gossip.hpp"
-#include "../../include/location/HotMeal.hpp"
 
 OrderBeerSuccess::OrderBeerSuccess(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state, const std::string& description, const std::string& choice_1, const std::string& choice_2, const std::string& choice_3)
     : InteractionWithNPC(player, game_state, description, choice_1, choice_2, choice_3) {
