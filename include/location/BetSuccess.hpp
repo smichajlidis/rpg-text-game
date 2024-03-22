250addbc4ee2393d2c5bf802e0849c66fa774ca4
smichajlidis <smichajlidis@gmail.com>
2024-03-22 17:10:43 +0100

    Connect with proper locations

--- include/location/BetSuccess.hpp
+++ include/location/BetSuccess.hpp
@@ -6,10 +6,10 @@
 class BetSuccess: public InteractionWithNPC {
 
 public:
-    BetSuccess(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state = nullptr, const std::string& description = "Lucky you, you won twice as much as you bet!",
+    BetSuccess(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state = nullptr,
+        const std::string& description = "Lucky you, you won twice as much as you bet!",
         const std::string& choice_1 = "Bet one more time",
-        const std::string& choice_2 = "Say goodbye")
-        : InteractionWithNPC(player, game_state, description, choice_1, choice_2) {}
+        const std::string& choice_2 = "Say goodbye");
     
     ~BetSuccess() = default;
 };
---
+++ src/location/BetSuccess.cpp
@@ -0,0 +1,10 @@
+#include "../../include/location/BetSuccess.hpp"
+
+BetSuccess::BetSuccess(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state, const std::string& description, const std::string& choice_1, const std::string& choice_2)
+    : InteractionWithNPC(player, game_state, description, choice_1, choice_2) {
+
+    npc = game_state->getNPC("gamblers");
+
+    related_locations.push_back("approaching_gamblers");
+    related_locations.push_back("tavern");
+}
