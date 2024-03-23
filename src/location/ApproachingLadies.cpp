96f3ec0e595a8c8aca32f925c450c82b8315ba50
smichajlidis <smichajlidis@gmail.com>
2024-03-23 15:40:33 +0100

    Connect related locations and ladies

--- src/location/ApproachingLadies.cpp
+++ src/location/ApproachingLadies.cpp
@@ -1,7 +1,21 @@
 #include "../../include/location/ApproachingLadies.hpp"
+#include "../../include/location/GoUpstairs.hpp"
+#include "../../include/location/Flirting.hpp"
 
 ApproachingLadies::ApproachingLadies(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state, const std::string& description, const std::string& choice_1, const std::string& choice_2, const std::string& choice_3)
     : InteractionWithNPC(player, game_state, description, choice_1, choice_2, choice_3) {
+
+    std::shared_ptr<NPC> ladies = std::make_shared<NPC>();
+    game_state->addNPC("ladies", ladies);
+    npc = game_state->getNPC("ladies");
+
+    game_state->addLocation("go_upstairs", std::make_shared<GoUpstairs>(game_state->getPlayer(), game_state));
+    game_state->addLocation("flirting", std::make_shared<Flirting>(game_state->getPlayer(), game_state));
+
+    related_locations.push_back("go_upstairs");
+    related_locations.push_back("flirting");
+    related_locations.push_back("tavern");
+
     sentences.push_back("- Guess what We are not wearing right now?");
     sentences.push_back("- Olive juice. Say it fast");
     sentences.push_back("- Hey, stranger! Wait, why are we still strangers? Let's fix that...");
