9e1cf47af6af53d50647bf6fe43d3d85690bf955
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-04-02 10:46:41 +0200

    Use operator << overload to list items in printEquipment()

--- src/LivingBeing.cpp
+++ src/LivingBeing.cpp
@@ -19,7 +19,8 @@ void LivingBeing::printEquipment() const {
     std::uint32_t count {1};
     if (!equipment.empty()) {
         std::for_each(equipment.begin(), equipment.end(), [&count](const Item& item) { 
-            std::cout << count << ". " << item.getName() << std::endl;
+            //std::cout << count << ". " << item.getName() << std::endl;
+            std::cout << count << ". " << item << std::endl;
             ++count;
         });
     }
