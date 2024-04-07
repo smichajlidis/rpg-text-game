0ac54f2d4fa5a415d4ae988b86f61bc5b9bfa96d
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-04-07 18:32:36 +0200

    add created items to game_state items map

--- src/location/ForestMeetEnemy.cpp
+++ src/location/ForestMeetEnemy.cpp
@@ -14,16 +14,19 @@ ForestMeetEnemy::ForestMeetEnemy(std::shared_ptr<Player> player, std::shared_ptr
 
     std::shared_ptr<LivingBeing> wolf = std::make_shared<LivingBeing>(10, "wolf");
     Item fur ("fur", 15);
+    game_state->addItem("fur", fur);
     wolf->addItem(fur);
     game_state->addEnemy("wolf", wolf);
     std::shared_ptr<LivingBeing> bear = std::make_shared<LivingBeing>(30, "bear");
     bear->addItem(fur);
     Item tusk ("tusk", 30);
+    game_state->addItem("tusk", tusk);
     bear->addItem(tusk);
     game_state->addEnemy("bear", bear);
     std::shared_ptr<LivingBeing> bandit = std::make_shared<LivingBeing>(12, "bandit");
     Item rusty_dagger ("rusty_dagger", 35, 10);
     bandit->addItem(rusty_dagger);
+    game_state->addItem("rusty_dagger", rusty_dagger);
     game_state->addEnemy("bandit", bandit);
 
     related_enemies.push_back("wolf");
