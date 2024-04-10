f34ea0ad534f545e7f2fecc5a7faa4cf50d969c3
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-04-10 16:56:08 +0200

    update getNextLocationName() by better rand logic

--- include/location/ForestFindDeerSuccess.hpp
+++ include/location/ForestFindDeerSuccess.hpp
@@ -7,7 +7,7 @@ class ForestFindDeerSuccess: public InteractionWithNPC {
 
 public:
     ForestFindDeerSuccess(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state = nullptr,
-        const std::string& description = "As you traverse through the forest, you stumble upon a deer, prompting you to draw your bow from your inventory and consider a potential hunt.",
+        const std::string& description = "As you journey through the forest, you catch a glimpse of movement among the trees, signaling the presence of potential prey. Reacting swiftly, you retrieve your bow from your inventory, considering the possibility of a successful hunt.",
         const std::string& choice_1 = "Try to hunt",
         const std::string& choice_2 = "Move forward",
         const std::string& choice_3 = "Enough exploring");
--- src/location/ForestFindDeerSuccess.cpp
+++ src/location/ForestFindDeerSuccess.cpp
@@ -1,4 +1,6 @@
 #include "../../include/location/ForestFindDeerSuccess.hpp"
+#include "../../include/location/HuntingSuccess.hpp"
+#include "../../include/location/HuntingFail.hpp"
 
 #include <iostream>
 #include <ctime>
@@ -7,6 +9,9 @@
 ForestFindDeerSuccess::ForestFindDeerSuccess(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state, const std::string& description, const std::string& choice_1, const std::string& choice_2, const std::string& choice_3)
     : InteractionWithNPC(player, game_state, description, choice_1, choice_2, choice_3) {
 
+    game_state->addLocation("hunting_success", std::make_shared<HuntingSuccess>(game_state->getPlayer(), game_state));
+    game_state->addLocation("hunting_fail", std::make_shared<HuntingFail>(game_state->getPlayer(), game_state));
+
     related_locations.push_back("hunting_success");
     related_locations.push_back("hunting_fail");
     related_locations.push_back("forest_exploration");
@@ -14,10 +19,11 @@ ForestFindDeerSuccess::ForestFindDeerSuccess(std::shared_ptr<Player> player, std
 }
 
 std::string ForestFindDeerSuccess::getNextLocationName(std::uint32_t val) {
-        if (val == 1) {
-            srand(time(NULL));
-            bool temp = rand() % 2;
-            return (temp ? "hunting_success" : "hunting_fail");
-        }
-        return related_locations.at(val);
+    if (val == 1) {
+        srand(time(NULL));
+        std::uint32_t max_score = 50;
+        std::uint32_t randomValue = rand() % 100 + 1;
+        return ((randomValue >= max_score - player->getLuck()) ? "hunting_success" : "hunting_fail");
+    }
+    return related_locations.at(val);
 }
