23da1ec175fca7676369f67f27fcadac56153f53
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-04-15 14:24:35 +0200

    create fighting_sentences and two methods() to get and add sentences

--- include/NPC.hpp
+++ include/NPC.hpp
@@ -3,19 +3,25 @@
 
 #include "LivingBeing.hpp"
 
+#include <vector>
+
 class NPC: public LivingBeing {
 
 public:
-    NPC() { gold = 300; attitude_to_player = 40; }
+    NPC(std::uint32_t strength = 10, std::string name = "npc")
+        : LivingBeing(strength, name) { gold = 300; attitude_to_player = 40; }
 
     ~NPC() = default;
 
     void increaseAttitudeToPlayer(std::uint16_t);
     void decreaseAttitudeToPlayer(std::uint16_t);
     std::uint16_t getAttitudeToPlayer() const;
+    std::string getFightingSentence() const;
+    void addFightingSentence(const std::string&);
 
 private:
     std::uint16_t attitude_to_player;
+    std::vector<std::string> fighting_sentences;
 };
 
 #endif
--- src/NPC.cpp
+++ src/NPC.cpp
@@ -1,5 +1,8 @@
 #include "../include/NPC.hpp"
 
+#include <ctime>
+#include <cstdlib>
+
 void NPC::increaseAttitudeToPlayer(std::uint16_t val) {
     attitude_to_player = ((attitude_to_player + val <= 100) ? attitude_to_player + val : 100);
 }
@@ -10,4 +13,16 @@ void NPC::decreaseAttitudeToPlayer(std::uint16_t val) {
 
 std::uint16_t NPC::getAttitudeToPlayer() const {
     return attitude_to_player;
+}
+
+std::string NPC::getFightingSentence() const {
+    srand(time(NULL));
+    if (!fighting_sentences.empty()) {
+        return fighting_sentences.at(std::rand() % fighting_sentences.size());
+    }
+    return "";
+}
+
+void NPC::addFightingSentence(const std::string& sentence) {
+    fighting_sentences.push_back(sentence);
 }
