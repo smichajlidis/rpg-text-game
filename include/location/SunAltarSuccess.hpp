1b95040c9e6843f7712f71e76df3d2bb50ab6c92
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-04-20 18:43:07 +0200

    create basic SunAltarSuccess without proper description yet

---
+++ include/location/SunAltarSuccess.hpp
@@ -0,0 +1,17 @@
+#ifndef _SUNALTARSUCCESS_HPP_
+#define _SUNALTARSUCCESS_HPP_
+
+#include "../InteractionWithNPC.hpp"
+
+class SunAltarSuccess: public InteractionWithNPC {
+
+public:
+    SunAltarSuccess(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state = nullptr,
+        const std::string& description = "description",
+        const std::string& choice_1 = "Keep praying (100 gold)",
+        const std::string& choice_2 = "Return");
+
+    ~SunAltarSuccess() = default;
+};
+
+#endif
