419e400138d27a38a10d8ea1105e6fb45224b0ca
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-04-22 13:11:37 +0200

    Create logic without proper description

---
+++ include/location/MysteriousAltarSuccess.hpp
@@ -0,0 +1,17 @@
+#ifndef _MYSTERIOUSALTARSUCCESS_HPP_
+#define _MYSTERIOUSALTARSUCCESS_HPP_
+
+#include "../InteractionWithNPC.hpp"
+
+class MysteriousAltarSuccess: public InteractionWithNPC {
+
+public:
+    MysteriousAltarSuccess(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state = nullptr,
+        const std::string& description = "description of mysterious altar",
+        const std::string& choice_1 = "Keep praying (100 gold)",
+        const std::string& choice_2 = "Return");
+
+    ~MysteriousAltarSuccess() = default;
+};
+
+#endif
