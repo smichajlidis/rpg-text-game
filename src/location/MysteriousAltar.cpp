f129da3e238387a8a67e13318acf70950f872ae2
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-04-25 18:48:21 +0200

    delete initialization of NoMoneyForPray

--- src/location/MysteriousAltar.cpp
+++ src/location/MysteriousAltar.cpp
@@ -1,12 +1,10 @@
 #include "../../include/location/MysteriousAltar.hpp"
 #include "../../include/location/MysteriousAltarSuccess.hpp"
-#include "../../include/location/NoMoneyForPray.hpp"
 
 MysteriousAltar::MysteriousAltar(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state)
     :InteractionWithNPC(player, game_state) {
 
     game_state->addLocation("mysterious_altar_success", std::make_shared<MysteriousAltarSuccess>(game_state->getPlayer(), game_state));
-    game_state->addLocation("no_money_for_pray", std::make_shared<NoMoneyForPray>(game_state));
 
     related_locations.push_back("mysterious_altar_success");
     related_locations.push_back("no_money_for_pray");
--- src/location/SunAltar.cpp
+++ src/location/SunAltar.cpp
@@ -6,7 +6,7 @@ SunAltar::SunAltar(std::shared_ptr<Player> player, std::shared_ptr<GameState> ga
     :InteractionWithNPC(player, game_state) {
 
     game_state->addLocation("sun_altar_success", std::make_shared<SunAltarSuccess>(game_state->getPlayer(), game_state));
-    game_state->addLocation("no_money_for_pray", std::make_shared<NoMoneyForPray>(game_state));
+    game_state->addLocation("no_money_for_pray", std::make_shared<NoMoneyForPray>(game_state->getPlayer(), game_state));
 
     related_locations.push_back("sun_altar_success");
     related_locations.push_back("no_money_for_pray");
