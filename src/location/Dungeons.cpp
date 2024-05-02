8adb002534c84164502cf0d1eadabceef47c40ee
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-05-02 14:35:49 +0200

    active DungeonsFindItem

--- src/location/Dungeons.cpp
+++ src/location/Dungeons.cpp
@@ -1,6 +1,6 @@
 #include "../../include/location/Dungeons.hpp"
 #include "../../include/location/DungeonsMeetEnemy.hpp"
-//#include "../../include/location/DungeonsFindItem.hpp"
+#include "../../include/location/DungeonsFindItem.hpp"
 #include "../../include/location/DungeonsNothingHappened.hpp"
 
 #include <ctime>
@@ -8,11 +8,11 @@
 Dungeons::Dungeons(std::shared_ptr<Player> player, std::shared_ptr<GameState> game_state)
     : InteractionWithNPC(player, game_state) {
 
-    //game_state->addLocation("dungeons_find_item", std::make_shared<DungeonsFindItem>(game_state->getPlayer(), game_state));
+    game_state->addLocation("dungeons_find_item", std::make_shared<DungeonsFindItem>(game_state->getPlayer(), game_state));
     game_state->addLocation("dungeons_meet_enemy", std::make_shared<DungeonsMeetEnemy>(game_state->getPlayer(), game_state));
     game_state->addLocation("dungeons_nothing_happened", std::make_shared<DungeonsNothingHappened>(game_state->getPlayer(), game_state));
 
-    //related_locations.push_back("dungeons_find_item");
+    related_locations.push_back("dungeons_find_item");
     related_locations.push_back("dungeons_meet_enemy");
     related_locations.push_back("dungeons_nothing_happened");
 }
