9886b651164003446e824a33dae6074db760b967
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-05-04 18:23:45 +0200

    define getType() method and redefine operator<< depends of types

--- src/Item.cpp
+++ src/Item.cpp
@@ -4,11 +4,24 @@
 
 std::ostream& operator<<(std::ostream& os, const Item& rhs) {
     os << rhs.name << " | value: " << rhs.value;
-    if (rhs.strength) {
-        os << " | strength: " << rhs.strength;
+    if (rhs.type == "food") {
+        os << " | Restore " << rhs.strength << " HP";
+    } else if (rhs.type == "weapon") {
+        os << " | Increase strength by " << rhs.strength;
+    } else if (rhs.type == "armor") {
+        os << " | Increase HP by " << rhs.strength;
+    } else if (rhs.type == "charisma_amulet") {
+        os << " | Increase charisma by " << rhs.strength;
+    } else if (rhs.type == "hp_amulet") {
+        os << " | Restore " << rhs.strength << " HP";
+    } else if (rhs.type == "strength_amulet") {
+        os << " | Increase strength by " << rhs.strength;
+    } else if (rhs.type == "luck_amulet") {
+        os << " | Increase luck by " << rhs.strength;
     }
     return os;
 }
+
 
 std::string Item::getName() const {
     return name;
@@ -16,6 +29,10 @@ std::string Item::getName() const {
 
 bool Item::empty() const {
     return ((getName() == "") ? true : false);
+}
+
+std::string Item::getType() const {
+    return type;
 }
 
 std::uint16_t Item::getStrength() const {
