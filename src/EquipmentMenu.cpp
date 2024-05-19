80a024c1625fd7e560693be2355a6bdb842caca8
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-05-19 20:10:10 +0200

    develop menu to make possible to choice item's options

--- src/EquipmentMenu.cpp
+++ src/EquipmentMenu.cpp
@@ -2,15 +2,45 @@
 #include "../include/Player.hpp"
 
 #include <iostream>
+#include <sstream>
 
 void EquipmentMenu::printMenu() {
-    char choice {};
-    header_displayer.displayHeader();
-    player->printEquipment();   
-    std::cout<<"\nPress any key to continue: ";
-    std::cin>>choice;
+    std::uint32_t choice;
+    do {
+        screen_stuff.clear();
+        header_displayer.displayHeader();
+        player->printEquipment();
+        std::cout << player->getEquipmentSize() + 1 << ". Return\n";   
+        std::cout << "\nYour choice: ";
+
+        std::string input;
+        std::cin >> input;
+        std::istringstream iss(input);
+
+        if (iss >> choice && choice <= player->getEquipmentSize() + 1) {
+            if (choice != player->getEquipmentSize() + 1) {
+                printChoices();
+                std::cout << "\nYour choice: ";
+                std::cin >> input;
+                std::istringstream iss(input);
+                if (iss >> choice) {
+                    action(choice);
+                    choice = {};
+                }
+            }
+        }
+    } while (choice != player->getEquipmentSize() + 1);
 }
 
-void EquipmentMenu::action(std::uint32_t) const {
-    
+void EquipmentMenu::action(std::uint32_t val) const {
+    switch (val) {
+        case 1: 
+            std::cout << "Using" << std::endl;
+            break;
+        case 2:
+            std::cout << "Dropping" << std::endl;
+            break;
+        default:
+            break;
+    }
 }
