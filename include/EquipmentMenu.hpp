70822f4ba52f6af78b3f0809d64fcaedcf52ce22
Slawomir Michajlidis <smichajlidis@gmail.com>
2024-05-19 20:08:48 +0200

    add some choices to ctor

--- include/EquipmentMenu.hpp
+++ include/EquipmentMenu.hpp
@@ -1,16 +1,21 @@
 #ifndef _EQUIPMENTMENU_HPP_
 #define _EQUIPMENTMENU_HPP_
 
+#include "ScreenStuff.hpp"
 #include "Menu.hpp"
 
 class EquipmentMenu: public Menu {
 public:
-    EquipmentMenu() = default;
+    EquipmentMenu(const std::string& title = "", const std::string& choice_1 = "Use", const std::string& choice_2 = "Drop", const std::string& choice_3 = "Return")
+    : Menu(title, choice_1, choice_2, choice_3) {}
 
     ~EquipmentMenu() = default;
 
     void printMenu() override;
     void action(std::uint32_t) const override;
+
+private:
+    ScreenStuff screen_stuff;
 };
 
 #endif
